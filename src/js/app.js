c666c3e2b7f6dae7d01e653e514eb14175ac325f
Natalia Baida <nnbaida@yandex.com>
2025-11-02 17:06:23 +0300

    fix: correct relative import paths in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 // В модуле app.js одним импортом импортируйте Game, GameSavingData и функции readGameSaving, writeGameSaving (их при импорте переименуйте в loadGame и saveGame соответственно)
-import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './js/game.js';
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
 
 const game = new Game();
 game.start();
